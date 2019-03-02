6d8f41c60c7ed5c67f5d8634eb885701d224d68e
Riku Sakamoto <rikusaka03@gmail.com>
2019-03-03 03:26:05 +0900

    音楽再生する仕組みを追加

--- Interface/music.js
+++ Interface/music.js
@@ -1,34 +1,12 @@
 "use strict";
 
-var page = {
- 
-  ready : false,
-   
-  play : function()
-  {
-    if(page.ready)
-    {
-      page.wav.play();
-    }
-  },
-   
-  stop : function()
-  {
-    if(page.ready)
-    {
-      page.wav.stop();
-    }
-  },
-   
-  page.onload = function() // body 要素の on_load 属性から呼ばれる
-  {
-    page.wav = new Audio('music/02_Money,Money,Money.m4a');
-    page.wav.onload = function()
-    {
-      document.getElementById("page_play").disabled=false;
-      document.getElementById("page_stop").disabled=false;
-      page.ready = true;
-    }
-  },
-   
-  };
+var as = audiojs.createAll();
+var audio = as[0];
+
+document.getElementById("MusicStart").addEventListener('click',function(){
+  audio.play();
+});
+
+document.getElementById("MusicStop").addEventListener('click',function(){
+  audio.pause();
+});
